a14166779a8b4096004c383e876626a01e1455de
Jeongwon Choi <chlwjddnjs17@naver.com>
2023-09-07 21:43:12 +0900

    인프런 자바 알고리즘 / 문장 속 단어(indexOf(), substring())

---
+++ src/pkg/Main.java
@@ -0,0 +1,32 @@
+package pkg;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public String solution(String str) {
+		String answer = "";
+		
+		int max = Integer.MIN_VALUE, pos;
+		 
+		while((pos = str.indexOf(' ')) != -1) {
+			String tmp = str.substring(0, pos);
+			if(tmp.length() > max) {
+				max = tmp.length();
+				answer = tmp;
+			}
+			str = str.substring(pos+1);
+		}
+		if(str.length() > max) answer=str;
+		
+		return answer;
+	} 
+	
+	public static void main(String[] args) {
+		Main T = new Main();
+		Scanner sc = new Scanner(System.in);
+		String str = sc.nextLine();
+		System.out.println(T.solution(str));
+	}
+
+}
